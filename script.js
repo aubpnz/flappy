55fefcb95cb8a65bf068e76f54680fdcb7a241b2
aubpnz <panzu.aubin@gmail.com>
2018-03-15 09:17:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function Saut() {
+$('#flappy').css({
+    'margin-bottom' : '300px'
+});
+}
+
+$(document).keydown(function(e){
+    if (e.keyCode == 38){
+        e.preventDefault();
+        $("#flappy").animate({marginBottom: '+=150px'},speed=100, easing="swing");
+    }
+})
+
+$(document).keydown(function(e){
+    if (e.keyCode == 39){
+        e.preventDefault();
+        $("#flappy").animate({marginLeft: '+=150px'},speed=100, easing="swing");
+    }
+})
+
+$(document).keydown(function(e){
+    if (e.keyCode == 40){
+        e.preventDefault();
+        $("#flappy").animate({marginTop: '+=150px'},speed=100, easing="swing");
+    }
+})
+
+$(document).keydown(function(e){
+    if (e.keyCode == 37){
+        e.preventDefault();
+        $("#flappy").animate({marginRight: '+=150px'},speed=100, easing="swing");
+    }
+});
+
+$(document).keydown(function(e){
+});
+
+    $("#pipe").animate({marginRight: "+=520px"},speed=2080);
